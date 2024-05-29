b2f45f3fb7ccb87d1026c5b80b8866dd1cc9bbbd
triple1566 <ljhub1566@gmail.com>
2024-05-29 15:16:18 -0400

    complete social links compo

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,19 +5,68 @@ import {BsFillPersonLinesFill} from'react-icons/bs';
 
 
 const SocialLinks = () => {
+
+  const links=[
+    {
+      id:1,
+      child:(
+        <>
+          LinkedIn<FaLinkedin size={30}/>
+        </>
+      ),
+      href: 'https://www.linkedin.com/in/leoj1566/',
+      style: 'rounded-tr-md'
+    },
+    {
+      id:2,
+      child:(
+        <>
+          Github<FaGithub size={30}/>
+        </>
+      ),
+      href: 'https://github.com/triple1566',
+      style: 'rounded-tr-md'
+    },
+    {
+      id:3,
+      child:(
+        <>
+          Mail<HiOutlineMail size={30}/>
+        </>
+      ),
+      href: 'mailto:ljhub1566@gmail.com',
+      style: 'rounded-tr-md'
+    },
+    {
+      id:4,
+      child:(
+        <>
+          Resume<BsFillPersonLinesFill size={30}/>
+        </>
+      ),
+      href: '/ResumeV10.pdf',
+      style: 'rounded-tr-md',
+      download: true,
+    }
+  ];
+
   return (
-    <div className='flex flex-col top-{35%} left-0 fixed'>
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        <li className='flex justify-between items-center w-40 h-1/4 px-4 bg-black'>
-          <a href="" className='flex justify-between items-center w-full text-white'>
-            <>
-              LinkedIn<FaLinkedin size={30}/>
-            </>
-          </a>
-        </li>
+        {links.map(({id, child, href, style, download}) => (
+
+            <li key = {id} className={"flex justify-between items-center w-40 h-14 px-4 bg-{#FFFFFF} ml-[-100px] hover:rounded-md duration-300 hover:ml-[-10px]" + " " + style}>
+              <a href={href} className='flex justify-between items-center w-full text-{#0c0e3e} ' download={download} target='_blank'>
+                <>
+                  {child}
+                </>
+              </a>
+            </li>))}
+
+
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default SocialLinks
