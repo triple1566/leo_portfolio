b537e2a17a6322412bf07a2f2c8e1ac419d4c3ee
triple1566 <ljhub1566@gmail.com>
2024-06-14 16:54:06 -0400

    added more images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import SocialLinks from './components/SocialLinks';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import Experience from './components/Experience';
 //import Three from './Three';
 
 
@@ -40,6 +41,7 @@ function App() {
         <br />
         <br />
       <Portfolio />
+      <Experience />
       <SocialLinks />
     </div>
   );
