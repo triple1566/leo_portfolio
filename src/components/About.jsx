ce5d481756a279d8ef435f8fe462dc378651dbdb
triple1566 <ljhub1566@gmail.com>
2024-08-07 15:34:24 -0400

    changed my introduction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,15 +11,7 @@ const About = () => {
                 </p>
             </div>
             <p className='text-xl mt-20'>
-            Welcome to my portfolio website! I'm an avid Unreal Engine developer with a strong passion for creating immersive and dynamic experiences. Currently, I'm deep into the development of an exciting FPS game, leveraging my expertise in Unreal Engine and its powerful Blueprint system to bring my vision to life.
-            </p>
-            <p><br /></p>
-            <p className='text-xl'>
-            With a solid foundation in frontend development, I create interactive web interfaces using React.js and JavaScript. My programming skills extend to Python, C, C++, and SQL, allowing me to tackle complex problems and deliver robust solutions across various platforms.
-            </p>
-            <p><br /></p>
-            <p className='text-xl'>
-            Explore my portfolio to see the projects that showcase my skills and creativity. Whether it's game development, frontend design, or backend logic, I am dedicated to pushing the boundaries of technology and delivering top-notch results. Let's create something extraordinary together!
+            Welcome to my portfolio website! I'm an aspiring student developer with a strong passion for creating immersive and dynamic experiences in web, games, and applications. Currently, I'm deep into the development of an exciting FPS game, leveraging my expertise in Unreal Engine and its powerful Blueprint system to bring my vision to life.
             </p>
         </div>
     </div>
