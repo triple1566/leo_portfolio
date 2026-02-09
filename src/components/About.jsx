d2fb6f7182c0a1a6975c728dad506ecd6eb2b94b
triple1566 <ljhub1566@gmail.com>
2026-02-09 18:59:52 +0900

    msg to all other jedis

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,7 @@ const About = () => {
                 </p>
             </div>
             <p className='text-xl mt-20'>
-            I’m a Computer Science student at the University of Toronto with a passion for AI, software development, and cloud computing ☁️💡. I specialize in machine learning, full-stack development, and scalable cloud solutions, combining technical expertise with innovative problem-solving.
-
-From building an RBF-based image denoiser 🖼️ and a React Native eBay listing app 📱 to optimizing cloud infrastructure, I love tackling complex challenges with efficient, data-driven solutions ⚡.
-
-Let’s connect and create something impactful! 🚀
+            Hi I'm Leo. Most of this stuff here is pretty outdated, so I'll be putting up a revamped portfolio soon. In the meantime, drink a cup of coffee.
             </p>
         </div>
     </div>
